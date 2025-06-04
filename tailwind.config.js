f0d5bccc7a44729f8e29d9b4fa9b85e6a1596d59
Risa Yamamoto <yamari2422@gmail.com>
2025-06-04 16:22:31 +0900

    feat: add variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Noto Sans JP"', "sans-serif"],
+        roboto: ['"Roboto"', "sans-serif"],
+        raleway: ['"Raleway"', "sans-serif"],
+        lustria: ['"Lustria"', "serif"],
+      },
+      colors: {
+        background: "#FFFDF7",
+        primary: "#D8B259",
+        "primary-light": "rgba(216, 178, 89, 0.48)",
+        secondary: "rgba(168, 187, 162, 0.48)",
+        text: "#37383C",
+        "text-sub": "#555555",
+        border: "#BFC0C0",
+      },
+    },
   },
   plugins: [],
 };
