8ad7fd3a1511c8f9490b0215b848642e96bd9d64
Risa Yamamoto <yamari2422@gmail.com>
2025-06-07 13:51:14 +0900

    chore: add the background section

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,13 +1,15 @@
-import React from "react";
-
-const SectionTitle = ({ title }) => {
+const SectionTitle = ({ title, sub = false }) => {
   return (
     <div className="flex items-center space-x-4">
       <div className="relative">
         <h2 className="relative z-10 text-3xl font-raleway whitespace-nowrap pl-4 pt-[0.8px]">
           {title}
         </h2>
-        <div className="absolute top-0 left-0 w-9 h-9 bg-primary-light rounded-full z-0"></div>
+        <div
+          className={`absolute top-0 left-0 w-9 h-9 rounded-full z-0 ${
+            sub ? "bg-secondary-light" : "bg-primary-light"
+          }`}
+        ></div>
       </div>
       <div className="flex-1 h-[0.8px] bg-border" />
     </div>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,38 +1,86 @@
 import SectionTitle from "../components/SectionTitle";
 
-const Profile = () => {
-  const profileItems = [
-    { text: "鹿児島県出身 🍠" },
-    { text: "カナダ在住 🇨🇦" },
-    { text: "1997年生まれ 🗓️" },
-  ];
+const profileItems = ["鹿児島県出身 🍠", "カナダ在住 🇨🇦", "1997年生まれ 🗓️"];
 
+const backgrounds = [
+  {
+    title: "Cornerstone International Community College of Canada",
+    date: "2024 - 現在",
+    description:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in",
+  },
+  {
+    title: "株式会社ARS",
+    date: "2021 - 2024 (3年)",
+    description:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in",
+  },
+  {
+    title: "鹿児島大学",
+    date: "2016 - 2021 (5年)",
+    description: "法文学部 経済情報学科",
+  },
+];
+
+const Profile = () => {
   return (
-    <section className="flex-1">
-      <section>
-        <SectionTitle title="Profile" />
-        <div className="flex flex-col items-center justify-center md:items-start justify-center md:flex-row md:gap-8 px-10 mt-4 md:mt-8">
-          <div className="w-[300]px flex ">
-            <img src="profile.svg" alt="" className="w-full h-full" />
-          </div>
-          <div className="md:w-1/2 space-y-4 md:mt-16">
-            <p className="font-base text-sm">
-              元々エンジニアをしていましたが、デザインの世界に惹かれ、xxxxxx元々エンジニア
-              をしていましたが、デザインの世界に惹かれ、xxxxxx元々エンジニアをしていましたが、デザインの世界に惹かれ、xxxxxx元々エンジニア
-              をしていましたが、デザインの世界に惹かれ、xxxxxx元々エンジニアをしていましたが、デザインの世界に惹かれ、xxxxxx元々エンジニア
-              をしていましたが、デザインの世界に惹かれ、xxxxxx元々エンジニアをしていましたが、デザインの世界に惹かれ、xxxxxx元々エンジニア
-            </p>
-            <ul className="flex flex-col md:flex-row gap-2">
-              {profileItems.map((item, index) => (
-                <li
-                  key={index}
-                  className="hidden lg:flex items-center px-4 py-1.5 text-text-sub border border-border rounded-full text-sm bg-white"
-                >
-                  {item.text}
-                </li>
-              ))}
-            </ul>
-          </div>
+    <section className="flex-1 space-y-10">
+      {/* Profile セクション */}
+      <SectionTitle title="Profile" />
+      <div className="flex flex-col items-center md:items-start md:flex-row md:gap-8 px-10 mt-4 md:mt-8">
+        <div className="w-[300px]">
+          <img src="profile.svg" alt="Profile" className="w-full h-full" />
+        </div>
+        <div className="md:w-1/2 space-y-4 md:mt-16">
+          <p className="font-base text-sm leading-relaxed">
+            元々エンジニアをしていましたが、デザインの世界に惹かれ、xxxxxx
+            元々エンジニアをしていましたが、デザインの世界に惹かれ、xxxxxx
+            元々エンジニアをしていましたが、デザインの世界に惹かれ、xxxxxx
+          </p>
+          <ul className="flex flex-wrap gap-2">
+            {profileItems.map((text, index) => (
+              <li
+                key={index}
+                className="px-4 py-1.5 text-text-sub border border-border rounded-full text-sm bg-white"
+              >
+                {text}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      {/* Backgrounds セクション */}
+      <section className="px-6 md:px-10">
+        <SectionTitle title="Backgrounds" sub />
+        <div className="px-6 md:px-10 mt-4 md:mt-8 space-y-4">
+          {backgrounds.map((item, index) => (
+            <div key={index} className="flex gap-4">
+              <div className="flex flex-col items-center space-y-2 mt-1.5">
+                <div
+                  className={`w-3 h-3 rounded-full ${
+                    index % 2 === 0 ? "bg-tertiary" : "bg-secondary"
+                  }`}
+                />
+                <div
+                  className={`${
+                    index === backgrounds.length - 1
+                      ? "hidden"
+                      : "w-px flex-1 border-l-2 border-dotted border-gray-400 h-16 "
+                  }`}
+                />
+              </div>
+              <div className="flex-1 space-y-2 md:space-y-1">
+                <div className="flex flex-col gap-1 md:flex-row justify-between">
+                  <p className="font-me">{item.title}</p>
+                  <p className="text-sm text-[#66676B]">{item.date}</p>
+                </div>
+                <p className="text-sm text-text-sub leading-relaxed ">
+                  {item.description}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </section>
     </section>
