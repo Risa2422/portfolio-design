a863a8d34c6433565221ad6dd85512aa1ae8988e
Risa Yamamoto <yamari2422@gmail.com>
2025-07-07 14:45:58 +0900

    chore: added a hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,73 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 import { Link, NavLink, useLocation } from "react-router-dom";
 
 function Header() {
   const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
 
   const navLinks = [
     { to: "/", label: "Home" },
     { to: "profile", label: "Profile" },
   ];
 
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
-    <header className="flex justify-between items-center py-6 px-[5vw] sm:px-[10vw] mb-[1vw]">
+    <header className="relative flex justify-between items-center py-6 px-[5vw] sm:px-[10vw] mb-[1vw] z-50">
       <Link to="/" className="text-xl font-bold">
         LOGO
       </Link>
-      <nav>
-        <ul className="flex sm:space-x-10 space-x-6">
-          {navLinks.map(({ to, label }) => (
-            <NavLink
-              key={to}
-              to={to}
-              className={({ isActive }) => {
-                const isWorkPath = location.pathname.startsWith("/works");
-                const active = isActive || (to === "/" && isWorkPath);
+      <button
+        onClick={toggleMenu}
+        className="sm:hidden focus:outline-none"
+        aria-label="Toggle navigation"
+      >
+        {isOpen ? <X size={24} /> : <Menu size={24} />}
+      </button>
 
-                return `transition-all duration-600 underline-offset-8 decoration-[1.4px] underline hover:text-primary ${
-                  active
-                    ? "decoration-primary text-primary"
-                    : "decoration-transparent"
-                }`;
-              }}
-            >
-              {label}
-            </NavLink>
-          ))}
+      {/* Navigation */}
+      <nav
+        className={`
+    absolute top-16 w-1/4 right-6 bg-white px-[3vw] rounded border border-gray-300
+    sm:static sm:block sm:bg-transparent sm:px-0 sm:py-0 sm:border-none sm:rounded-none
+    transition-all duration-200
+    ${isOpen ? "opacity-100 scale-100 visible" : "opacity-0 scale-95 invisible"}
+    sm:opacity-100 sm:scale-100 sm:visible
+  `}
+      >
+        <ul className="flex flex-col sm:flex-row sm:space-x-10 sm:space-y-0 py-1 sm:py-0 justify-end">
+          {navLinks.map(({ to, label }, index) => {
+            const isWorkPath = location.pathname.startsWith("/works");
+
+            return (
+              <li
+                key={to}
+                className={`${
+                  index === 1 ? "border-t border-gray-300 sm:border-none" : ""
+                }`}
+              >
+                <NavLink
+                  to={to}
+                  onClick={() => setIsOpen(false)}
+                  className={({ isActive }) => {
+                    const active = isActive || (to === "/" && isWorkPath);
+                    return `
+              transition-all duration-600 underline-offset-8 decoration-[1.4px] sm:underline hover:text-primary space-y-4 text-sm sm:text-base block py-3 
+              ${
+                active && !isOpen
+                  ? "sm:decoration-primary text-primary"
+                  : "sm:decoration-transparent text-base"
+              }
+              ${active && isOpen ? "text-primary text-start" : "text-start"}
+            `;
+                  }}
+                >
+                  {label}
+                </NavLink>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </header>
