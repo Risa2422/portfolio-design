030139d56e0559afd8109d247f9c930dc8300333
Risa Yamamoto <yamari2422@gmail.com>
2025-07-07 20:40:38 +0900

    chore: edit descriptions of the background section

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -10,13 +10,13 @@ const backgrounds = [
     title: "Cornerstone International Community College of Canada",
     date: "2024 - 現在",
     description:
-      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in",
+      "Web開発の基礎からNext.jsなどのモダンな技術を用いたアプリ開発までを、1年をかけて体系的に学びました。授業外では、クライアントワークとしてWordPress（Elementor）を用いたWebサイトの制作や、ボランティア活動の一環として絵画オークションサービスのデザインを担当しました。",
   },
   {
     title: "株式会社ARS",
     date: "2021 - 2024 (3年)",
     description:
-      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in",
+      "自社サービスであるPOSシステム、患者動線管理システムの導入・開発・保守業務に携わっていました。 特に、システム導入時や保守対応の際には、お客様とのコミュニケーションを重視し、ニーズを的確に把握した上で可能な限り迅速に実現できるよう心がけていました。お客様ごとに仕様が異なる中で、それぞれの要望に柔軟に対応する力を培うことができました。",
   },
   {
     title: "鹿児島大学",
@@ -31,7 +31,14 @@ const Profile = () => {
       <SectionTitle title="Profile" />
       <div className="flex flex-col items-center justify-center md:items-start md:flex-row md:gap-16 px-10 md:px-0 mt-4 md:mt-10">
         <div className="w-[320px]">
-          <img src="profile.svg" alt="Profile" className="w-full h-full" />
+          <img
+            src="profile.svg"
+            alt="Profile"
+            width={320}
+            height={320}
+            className="w-full h-full"
+            loading="lazy"
+          />
         </div>
         <div className="md:w-1/2 space-y-5 md:mt-16">
           <p className="font-base text-sm leading-relaxed">
@@ -54,15 +61,16 @@ const Profile = () => {
       <div className="space-y-16 md:space-y-20 pb-10">
         <section className="px-6 md:px-4">
           <SectionTitle title="Backgrounds" sub />
-          <div className="px-4 md:px-10 mt-4 md:mt-8 space-y-8">
+          <div className="px-4 md:px-10 mt-4 md:mt-8 space-y-4">
             {backgrounds.map((item, index) => (
               <div key={index} className="flex gap-4">
                 <div className="flex flex-col items-center space-y-2 mt-1.5">
-                  <div
+                  {/* <div
                     className={`w-3 h-3 rounded-full ${
                       index % 2 === 0 ? "bg-tertiary" : "bg-secondary"
                     }`}
-                  />
+                  /> */}
+                  <img src="rhombus.svg" alt="" className="w-3.5 h-3.5" />
                   <div
                     className={`${
                       index === backgrounds.length - 1
